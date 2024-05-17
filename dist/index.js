4084c5de0c9f376c67964a439e6c58370f2a75cf
Matheus P Agostinho <contact@matheuspa.com>
2024-05-17 16:58:00 -0300

    new: build action

--- dist/index.js
+++ dist/index.js
@@ -29195,63 +29195,79 @@ const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 const { Octokit } = __nccwpck_require__(6762)
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+async function listDeployments(octokit, owner, repo) {
+  const response = await octokit.request(
+    'GET /repos/{owner}/{repo}/deployments',
+    {
+      owner,
+      repo
+    }
+  )
+  return response.data || []
+}
+
+async function changeStatusDeployment(octokit, owner, repo, id) {
+  const state = 'inactive'
+
+  const response = await octokit.request(
+    'POST /repos/{owner}/{repo}/deployments/{id}/statuses',
+    {
+      owner,
+      repo,
+      id,
+      data: {
+        state
+      }
+    }
+  )
+
+  return response.status === 200
+}
+
 async function run() {
   try {
     const token = core.getInput('token', { required: true })
     const owner = core.getInput('owner', { required: true })
     const repo = core.getInput('repo', { required: true })
 
-    // Get the branch that triggered the action
     const ref = github.context.ref
-    // const branch = ref.replace('refs/heads/', '')
+    const branch = 'fix/switch-header-and-footer'
 
-    // const branch = 'fix/switch-header-and-footer'
-    const branch = 'switch-header-and-footer'
+    console.log(`Branch: ${branch}`)
 
-    // Request for github
     const octokit = new Octokit({ auth: token })
-    const response = await octokit.request(
-      'GET /repos/{owner}/{repo}/deployments',
-      {
-        owner,
-        repo
-      }
-    )
 
-    const deployments = response.data
-
-    const findDeployment = deployments.find(deployment => {
+    const deployments = await listDeployments(octokit, owner, repo)
+    const currentBranchDeployment = deployments.find(deployment => {
       console.log('deployment', deployment.ref)
       console.log('branch', branch)
 
-      if (deployment.ref === branch) {
-        return deployment
-      }
+      return deployment.ref === branch
     })
 
-    if (!findDeployment) {
+    if (!currentBranchDeployment) {
       throw new Error(`No deployment found for branch: ${branch}`)
     }
 
-    console.log(findDeployment)
+    const result = await changeStatusDeployment(
+      octokit,
+      owner,
+      repo,
+      currentBranchDeployment.id
+    )
 
-    // Log the branch name to the console
-    console.log(`Branch: ${branch}`)
+    console.log('Result change status', result)
 
-    // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
 }
 
 module.exports = {
-  run
+  run,
+  listDeployments,
+  changeStatusDeployment
 }
 
 
